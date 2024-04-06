a9671aa772554cc1eb260b1988cb84349b32d82b
Lidiay1488 <142167357+Lidiay1488@users.noreply.github.com>
2024-04-06 13:44:48 +0300

    Update task63.py

--- dags/task63.py
+++ dags/task63.py
@@ -30,7 +30,7 @@ hello_file_operator = BashOperator(task_id='hello_file_task', bash_command='./sc
 task_http_sensor_check = HttpSensor(
     task_id='http_sensor_check',
     http_conn_id='test_connect',
-    endpoint='/posts',
+    endpoint='posts',
     request_params={},
     response_check=lambda response: 'httpbin' in response.text,
     poke_interval=5,
